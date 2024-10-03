6c615e94ff4bdafc5d1828782abbbffcebf373c4
martaducamp <martad@orange.fr>
2024-10-03 16:59:17 +0200

    creation .gitignore et modification dictionnaire

--- python/Grille.py
+++ python/Grille.py
@@ -5,7 +5,9 @@ class Grille:
     DIFFICULTE = {
         "facile": {"taille_grille": (9, 9), "mines": 10},
         "intermediaire": {"taille_grille": (16, 16), "mines": 40},
-        "avance": {"taille_grille": (30, 16), "mines": 99}
+        "avance": {"taille_grille": (30, 16), "mines": 99},
+        "surhomme": {"taille_grille": (50 ,50), "mines": 500},
+        "extraterrestre": {"taille_grille": (100, 100), "mines": 1000}
     }
     
     def __init__(self, longueur, largeur):
