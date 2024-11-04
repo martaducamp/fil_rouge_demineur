edc9853430d80d6b812413f37afd9c254f11be8b
martaducamp <martad@orange.fr>
2024-11-04 10:11:35 +0100

    commentaires

--- python/CaseNumero.py
+++ python/CaseNumero.py
@@ -3,15 +3,45 @@ from Case import Case
 
 class CaseNumero(Case):
     def __init__(self, x, y, minesAdjacentes):
+        """
+        Initialise une instance de CaseNumero, représentant une case sans mine 
+        mais avec un nombre de mines adjacentes.
+
+        Parameters
+        ----------
+        x : int
+            Coordonnée en x de la case sur la grille.
+        y : int
+            Coordonnée en y de la case sur la grille.
+        minesAdjacentes : int
+            Nombre de mines adjacentes à cette case.
+
+        Attributes
+        ----------
+        isDecouvert : bool
+            Indique si la case est découverte.
+        minesAdjacentes : int
+            Nombre de mines autour de cette case.
+        drapeau : bool
+            Indique si un drapeau est placé sur cette case.
+        isMine : bool
+            Toujours False pour CaseNumero, indique que la case ne contient pas de mine.
+        """
         super().__init__(x, y, isDecouvert=False, minesAdjacentes=minesAdjacentes, drapeau=False, isMine=False)
     
     def clicGauche(self):
         """
-        Action effectuée lors d'un clic gauche sur une case vide.
-        Dévoile la case si elle n'a pas de drapeau et qu'elle n'est pas déjà découverte.
+        Action effectuée lors d'un clic gauche sur une case sans mine.
+        
+        Si la case n'est pas déjà découverte et n'a pas de drapeau, elle est découverte
+        et son état `isDecouvert` passe à True.
+
+        Returns
+        -------
+        None
         """
         if not self.isDecouvert and not self.drapeau:
             self.isDecouvert = True
             # print(f"{self.minesAdjacentes} découvert à la position ({self.x}, {self.y})")
-        else:
-            print(f"Action impossible : {'drapeau présent' if self.drapeau else 'case déjà découverte'}")
+        # else:
+        #     print(f"Action impossible : {'drapeau présent' if self.drapeau else 'case déjà découverte'}")
--- python/Jeu.py
+++ python/Jeu.py
@@ -28,6 +28,16 @@ class Jeu:
     def initialiser_jeu(self):
         """
         Initialise la grille en fonction de la difficulté.
+
+        Raises
+        ------
+        ValueError
+            Erreur si la difficulté demandée n'est pas dans le dictionnaire.
+
+        Returns
+        -------
+        None.
+
         """
         # Création de la grille selon la difficulté
         params = Grille.DIFFICULTE.get(self.difficulte)
@@ -41,39 +51,24 @@ class Jeu:
         self.grille.creerGrille()
         self.grille.afficherGrille()
         self.timer.reset()
-
-    def jouer(self):
-        """
-        Démarre la boucle principale du jeu, permet au joueur d'entrer des coordonnées pour découvrir des cases.
-        """
-        # Initialisation du jeu
-        self.initialiser_jeu()
         
-        # Boucle principale du jeu
-        while True:
-            try:
-                x = int(input("Entrez la coordonnée x: "))
-                y = int(input("Entrez la coordonnée y: "))
-                action = str(input("Entrez le type d'action (f pour drapeau et d pour découvrir): "))
-            except ValueError:
-                print("Coordonnées invalides.")
-                continue
-            
-            # Traiter le coup du joueur
-            self.traiterCoup(x, y, action)
-            self.grille.afficherGrille()
-            
-            # Condition de victoire ou de fin de jeu à gérer ici
-            if self.grille.victoire():
-                print("Vous avez gagné !")
-                break
-            if self.grille.defaite(x,y):
-                print("Vous avez perdu :(")
-                break
-
-    def traiterCoup(self, x: int, y: int, action: str):
+    def traiterCoup(self, x, y, action):
         """
         Traite le coup du joueur. Découvre la case et place les mines si c'est la première action.
+
+        Parameters
+        ----------
+        x : int
+            coordonnée x de la case.
+        y : int
+            Coordonnée y de la case.
+        action : str
+            "d" pour découvrir la case, "f" pour placer un drapeau.
+
+        Returns
+        -------
+        None.
+
         """
         if self.premiereAction:
             self.timer.start()
@@ -100,6 +95,42 @@ class Jeu:
         
         if self.grille.victoire() or self.grille.defaite(x, y):
             self.timer.stop()
+
+    def jouer(self):
+        """
+        Démarre la boucle principale du jeu, permet au joueur d'entrer des coordonnées pour découvrir des cases.
+
+        Returns
+        -------
+        None.
+
+        """
+        # Initialisation du jeu
+        self.initialiser_jeu()
+        
+        # Boucle principale du jeu
+        while True:
+            try:
+                x = int(input("Entrez la coordonnée x: "))
+                y = int(input("Entrez la coordonnée y: "))
+                action = str(input("Entrez le type d'action (f pour drapeau et d pour découvrir): "))
+            except ValueError:
+                print("Coordonnées invalides.")
+                continue
+            
+            # Traiter le coup du joueur
+            self.traiterCoup(x, y, action)
+            self.grille.afficherGrille()
+            
+            # Condition de victoire ou de fin de jeu à gérer ici
+            if self.grille.victoire():
+                print("Vous avez gagné !")
+                break
+            if self.grille.defaite(x,y):
+                print("Vous avez perdu :(")
+                break
+
+
     
 
 
